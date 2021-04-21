0506108c880611bfbc5d982ba06a32f72f68e809
Binayak Reddy <77384106+BinayakReddy@users.noreply.github.com>
2021-04-21 20:12:06 +0530

    Add files via upload

---
+++ src/main/java/in/binayak/springboot/Springbooth2databaseApplication.java
@@ -0,0 +1,13 @@
+package in.binayak.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbooth2databaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbooth2databaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/binayak/springboot/Springbooth2databaseApplicationTests.java
@@ -0,0 +1,13 @@
+package in.binayak.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbooth2databaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
